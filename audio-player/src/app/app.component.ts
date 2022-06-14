82d66de6c843bc6d9497f8f21c28ca094d452f90
dev-ramkumar <ramkumarg1605@gmail.com>
2022-06-14 15:20:04 +0530

    code

--- audio-player/src/app/app.component.ts
+++ audio-player/src/app/app.component.ts
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'audio-player';
+  title = 'player';
+
+  keywords = ""
+  songs = new Array()
+
+  song= {
+    image: "",
+    title: "",
+    singer: "",
+    music: "",
+    movie: "",
+    lyrics: ""
+  }
+  index= 0
+  isPlaying = false
+
+  audio = new Audio();
+
+  playAudio(song: any, index: any) {
+    this.index = index
+    this.song = song
+    this.audio.src = song.path;
+    this.audio.load();
+    this.play()
+  }
+
+  play(){
+    this.audio.play();
+    this.isPlaying = true
+  }
+
+  pauseAudio () {
+    this.audio.pause()
+    this.isPlaying = false
+  }
+
+  previous() {
+    if(this.index == 0) {
+      this.playAudio(this.songs[this.songs.length - 1], this.songs.length - 1)
+    } else {
+      this.playAudio(this.songs[this.index - 1], this.index - 1)
+    }
+  }
+  next() {
+    if(this.index == (this.songs.length - 1)) {
+      this.playAudio(this.songs[0], 0)
+    } else {
+      this.playAudio(this.songs[this.index + 1], this.index + 1)
+    }
+  }
+
+  toggle() {
+    if(this.isPlaying) {
+      this.pauseAudio()
+    } else {
+      this.play()
+    }
+  }
+
+  search(e: any) {
+
+    fetch('https://ramkumarg1605.000webhostapp.com/telliant/search.php?search=' + e)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.songs = data
+      });
+
+  }
+
+  playSong(index: any) {
+    this.playAudio(this.songs[index], index)
+  }
 }
